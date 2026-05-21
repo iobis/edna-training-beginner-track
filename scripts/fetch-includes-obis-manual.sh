7e419c3d5920324d78b35af13205c860412d74e3
sformel <steve@thedatapatch.com>
2026-05-21 10:48:29 -0500

    updated include proof of concept

--- scripts/fetch-includes-obis-manual.sh
+++ scripts/fetch-includes-obis-manual.sh
@@ -11,5 +11,14 @@ FILES=(
 )
 
 for f in "${FILES[@]}"; do
-  wget -q -O "_includes/$f" "$BASE/$f"
+  curl -fsSL -o "_includes/obis-manual/$f" "$BASE/$f"
+done
+
+# Make R chunks fail soft: render errors inline instead of halting the build.
+# This is because fetched files may reference paths/resources that exist only
+# in the source repo (iobis/manual).
+for f in "${FILES[@]}"; do
+  sed -i.bak -E 's/^(```\{r[^}]*)\}/\1, error=TRUE, warning=TRUE\}/' \
+    "_includes/obis-manual/$f"
+  rm "_includes/obis-manual/$f.bak"
 done
